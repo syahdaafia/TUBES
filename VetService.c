99a039c90ddf12e380f5d92095ea8914fbd68458
syahdaafia <76998317+syahdaafia@users.noreply.github.com>
2021-07-14 18:14:16 +0700

    Create VetService.c

---
+++ VetService.c
@@ -0,0 +1,9 @@
+/*
+	Program		: VetService.c
+	Author		: Muhammad Faza Hardiansyah (201524016)
+				  Muhammad Fikri Hidayaulloh (201524017)
+				  Syahda Afia Dhiya Ulhaq Tajudin (201524030)
+	Kelompok	: A9
+	Deskripsi	: Aplikasi layanan Dokter Hewan (mendata, menampilkan, dan memanggil antrian)
+	Compiler	: DevC++ TDM-GCC 4.9.2 64-bit Release
+*/
