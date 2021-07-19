f73c6290245aada91be48409ff688953ac8967ec
syahdaafia <76998317+syahdaafia@users.noreply.github.com>
2021-07-19 23:00:12 +0700

    create operasi ADT

--- VetService.h
+++ VetService.h
@@ -10,8 +10,8 @@
 #ifndef VETSERVICE_H
 #define VETSERVICE_H
 
-/* Atribut ADT */
-/*=============================================================*/
+			/* Atribut ADT */
+/*========================================*/
 typedef struct NodeQueue *addrNQ;
 typedef struct InfoPelangganVetService infotype;
 
@@ -35,7 +35,83 @@ typedef struct Queue{
 	addrNQ rear;
 }Queue;
 
+		   	/* Operasi ADT */
+/*========================================*/
 
+/*** MANAJEMEN MEMORI ***/
+/* Mengirimkan address hasil alokasi sebuah elemen dengan info X.
+   Jika alokasi berhasil, modul mengembalikan P; P->info = X, = NULL.
+   P adalah pointer yang menunjuk ke node Queue sebagai hasil alokasi.
+   Jika alokasi gagal, modul mengembalikan NULL.
+ */
+addrNQ Alokasi (infotype X);
+
+/* Melakukan dealokasi elemen P (pointer menunjuk ke alamat node queue).
+   I.S.: P terdefinisi.
+   F.S.: P dikembalikan ke sistem.
+ */
+void Dealokasi (addrNQ *P);
+
+/*** Manajemen Queue ***/
+/* Membuat sebuah Queue kosong dengan Q.fornt = NULL dan Q.rear = NULL. 
+   I.S. Belum terbentuk Queue 
+   F.S. Sudah terbentuk Queue
+*/
+void BuatAntrian (Queue *Q);
+
+/* Mengetahui apakah Queue kosong atau tidak. 
+   mengirimkan 1, jika Queue kosong yaitu Q.front = NULL dan Q.rear = NULL.
+   mengirimkan 0, jika Queue tidak kosong.
+*/
+int CekAntrianKosong (Queue Q);
+
+/* Memasukkan info baru ke dalam Queue dengan aturan FIFO.
+   I.S. Q mungkin kosong atau Q mungkin berisi antrian.
+   F.S. info baru telah masuk ke dalam Queue.
+*/ 
+void Antrian (Queue *Q, infotype data);
+
+/* Menghapus node dari Queue dengan aturan FIFO .
+   I.S. Q mungkin kosong atau Q mungkin berisi antrian.
+   F.S. Node sudah terhapus.
+*/ 
+void MulaiService (Queue *Q, infotype *data);
+
+
+/*** MANAJEMEN WAKTU ***/
+/* Menghitung waktu service sesuai dengan kategori penyakit.
+   if penyakit ... masuk ke kategori ... return ...
+*/
+int HitungWaktuService (infotype X);
+
+/* Menghitung waktu selesai layanan. 
+   Mengembalikan hasil dari WDatang + WTunggu + WService.
+*/
+int HitungWaktuSelesai (infotype X);
+
+/* Menghitung waktu tunggu layanan.
+   Mengembalikan hasil dari WSelesai (Node sebelumnya)- WDaftar.
+*/
+int HitungWaktuTunggu (addrNQ P, infotype X);
+
+/* Menghitung waktu tunggu layanan .
+   Mengembalikan hasil dari WDatang + WTunggu.
+*/
+int HitungWaktuMulai (infotype X);
+
+
+/*** MANAJEMEN MENU ***/
+/* Memasukkan info pelanggan vet service.
+   I.S. info pelanggan vet service masih kosong.
+   F.S. info pelanggan vet service sudah terisi.
+*/ 
+void DaftarAntrian (Queue *Q);
+
+/* Menampilkan info pelanggan vet service.
+   I.S. info pelanggan vet service belum ditampilkan.
+   F.S. info pelanggan vet service sudah ditampilkan.
+*/ 
+void PrintAntrian (Queue Q);
 
 
 
